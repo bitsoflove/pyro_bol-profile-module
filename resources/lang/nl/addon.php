9b2d1c8b590b49f5ed020b1b27b65920e4bf6b31
Michiel <michiel@bitsoflove.be>
2016-10-13 14:04:10 +0200

    nl translations

---
+++ resources/lang/nl/addon.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'       => 'Profiel',
+    'name'        => 'Profiel Module',
+    'description' => 'Verander je profiel',
+    'section' => [
+        'profile' => 'Profiel'
+    ]
+];
---
+++ resources/lang/nl/field.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'new_password' =>[
+        'name' => "Nieuw wachtwoord"
+    ],
+    'old_password' =>[
+        'name' => "Oud wachtwoord"
+    ],
+    'confirm_password' =>[
+        'name' => "Bevestig wachtwoord"
+    ]
+];
